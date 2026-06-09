849bfa3241e98b4ae7ab4bed09c058de659f0610
Naufal Akbar Pinasthika <nauakbar76@gmail.com>
2026-06-09 14:29:15 +0700

    fix: allow collapsed sidebar vertical scroll

--- resources/js/components/ui/sidebar/SidebarContent.vue
+++ resources/js/components/ui/sidebar/SidebarContent.vue
@@ -67,7 +67,7 @@ onBeforeUnmount(() => {
     <div
         ref="sidebarContent"
         data-sidebar="content"
-        :class="cn('flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden', props.class)"
+        :class="cn('flex min-h-0 flex-1 flex-col gap-2 overflow-y-auto overflow-x-hidden', props.class)"
     >
         <slot />
     </div>
